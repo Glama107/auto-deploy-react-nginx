cea422d5a3431e8339c9fb3a6b3b37a170ac3f3c
Glama <guigui.mareschal@gmail.com>
2022-05-21 20:46:01 +0200

    ✅ Adding delete function style

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -5,6 +5,30 @@ clear
 #get current path
 path=$(pwd)
 
+successAscii="\033[0;32m
+
+    _____ _    _  _____ _____ ______  _____ _____ 
+    / ____| |  | |/ ____/ ____|  ____|/ ____/ ____|
+    | (___ | |  | | |   | |    | |__  | (___| (___  
+    \___ \| |  | | |   | |    |  __|  \___ \\___ \ 
+    ____) | |__| | |___| |____| |____ ____) |___) |
+    |_____/ \____/ \_____\_____|______|_____/_____/ 
+                                                    
+                                                    
+                                        
+    \033[0;0m"
+autoDeployText="\033[0;31m
+
+          _    _ _______ ____    _____  ______ _____  _      ______     __
+     /\  | |  | |__   __/ __ \  |  __ \|  ____|  __ \| |    / __ \ \   / /
+    /  \ | |  | |  | | | |  | | | |  | | |__  | |__) | |   | |  | \ \_/ / 
+   / /\ \| |  | |  | | | |  | | | |  | |  __| |  ___/| |   | |  | |\   /  
+  / ____ \ |__| |  | | | |__| | | |__| | |____| |    | |___| |__| | | |   
+ /_/    \_\____/   |_|  \____/  |_____/|______|_|    |______\____/  |_|   
+                                                                          
+                                                                                                                                          
+\033[0m"
+
 Help()
 {
    # Display Help
@@ -18,35 +42,11 @@ Help()
    echo
 }
 
-echo -e "\033[0;31m
+echo -e autoDeployText
 
-          _    _ _______ ____    _____  ______ _____  _      ______     __
-     /\  | |  | |__   __/ __ \  |  __ \|  ____|  __ \| |    / __ \ \   / /
-    /  \ | |  | |  | | | |  | | | |  | | |__  | |__) | |   | |  | \ \_/ / 
-   / /\ \| |  | |  | | | |  | | | |  | |  __| |  ___/| |   | |  | |\   /  
-  / ____ \ |__| |  | | | |__| | | |__| | |____| |    | |___| |__| | | |   
- /_/    \_\____/   |_|  \____/  |_____/|______|_|    |______\____/  |_|   
-                                                                          
-                                                                          
-                                                                        
-\033[0m"
-
-Deploy()
+CloneProject()
 {
-    echo 'Installing packages ...'
-    #Adding source for node16
-    curl -s https://deb.nodesource.com/setup_16.x | sudo bash > /dev/null 2>&1
-    sudo apt update > /dev/null 2>&1
-    sudo apt install nodejs npm git-all nginx -y > /dev/null 2>&1
-
-    clear
-
-    echo 'Do you have a Git repository ? [Y/N] : '
-    read repo
-
-    if [ $repo = 'Y' ] || [ $repo = 'Yes' ] || [ $repo = 'y' ] || [ $repo = 'yes' ]
-    then
-        #clone repo & define project name
+    #clone repo & define project name
         echo 'Paste your link here : '
         read gitrepo
         echo ""
@@ -70,7 +70,106 @@ Deploy()
                 echo -e "\033[0;31mERROR: Installation failed\033[0;0m"
                 exit 1
             fi
-        
+}
+
+Installation()
+{
+    #duplicate & rename config file to project name
+            sudo cp config $projectname
+
+            #templating modification in config file
+            sed -i "s/SERVERNAME/$hostip/" $projectname
+            sed -i "s/PROJECTNAME/$projectname/" $projectname
+
+            #create folder insite html folder & add permission of nginx to read folder
+            sudo mkdir /var/www/html/$projectname > /dev/null 2>&1
+            sudo chown -R www-data:www-data /var/www/html/$projectname > /dev/null 2>&1
+
+            #move config file to sites-available
+            sudo cp $projectname /etc/nginx/sites-available/ > /dev/null 2>&1
+
+            #move build project to html folder
+            sudo cp -a "${projectpath}/build/." /var/www/html/$projectname > /dev/null 2>&1
+
+            #make link between sites-available & site enabled
+            sudo ln -s /etc/nginx/sites-available/$projectname /etc/nginx/sites-enabled > /dev/null 2>&1
+
+            #delete default conf of nginx
+            sudo rm -f /etc/nginx/sites-available/default > /dev/null 2>&1
+            sudo rm -f /etc/nginx/sites-enabled/default > /dev/null 2>&1
+
+            #delete config file in current folder
+            sudo rm $projectname
+
+            echo -e successAscii
+
+            echo -e "File has been created in \033[0;36m/etc/nginx/sites-available/$projectname\033[0;0m"
+            echo -e "Your website folder is located in \033[0;36m/var/www/html/$projectname\033[0;0m"
+            echo -e "Link has been make in \033[0;36m/etc/nginx/site-enabled/$projectname\033[0;0m"
+            echo ""
+
+            sleep 1
+            
+            echo "Testing config for nginx..."
+            #test if config of nginx is correct or not
+            sudo nginx -t > /dev/null 2>&1
+            if [ $? -eq 0 ]
+            then
+                #if config is ok, restart nginx
+                sudo systemctl restart nginx > /dev/null 2>&1 
+                echo -e "\033[0;32mSUCCESS! Go to http://${hostip}/ to see you website\033[0;0m"
+            else 
+                echo -e "\033[0;31mERROR: Config Failed\033[0;0m"
+                exit 1
+            fi
+}
+SSLSetup()
+{
+    clear
+
+        echo "Installing packages ..."
+        sudo apt install certbot python3 python3-certbot-nginx -y > /dev/null 2>&1
+
+        echo "Put your Host Domain Name (eg: www.example.com)"
+        read dns
+
+        #edit server name in sites-available folder of nginx
+        sed -i "s/$hostip/$dns/" /etc/nginx/sites-available/$projectname > /dev/null 2>&1
+
+        #use certbot to link dns & to put HTTPS
+        sudo certbot --nginx -d $dns
+        if [ $? -eq 0 ]
+        then
+            clear
+            #restart nginx
+            echo "Restarting nginx..."
+            sudo systemctl restart nginx > /dev/null 2>&1
+
+            clear
+            echo -e "\033[0;32mSuccess !\033[0;0m go to \033[0;32mhttps://${dns}/\033[0;0m"
+            echo -e "You can test the config at \033[0;32mhttps://www.ssllabs.com/ssltest/analyze.html?d=${dns}&latest\033[0;0m"
+        else
+            echo -e "\033[0;31mError : unable to setup certbot certificate, try again.\033[0;0m"
+            exit 1
+        fi
+}
+
+Deploy()
+{
+    echo 'Installing packages ...'
+    #Adding source for node16
+    curl -s https://deb.nodesource.com/setup_16.x | sudo bash > /dev/null 2>&1
+    sudo apt update > /dev/null 2>&1
+    sudo apt install nodejs npm git-all nginx -y > /dev/null 2>&1
+
+    clear
+
+    echo 'Do you have a Git repository ? [Y/N] : '
+    read repo
+
+    if [ $repo = 'Y' ] || [ $repo = 'Yes' ] || [ $repo = 'y' ] || [ $repo = 'yes' ]
+    then
+        CloneProject
         
     elif [ $repo = 'N' ] || [ $repo = 'No' ] || [ $repo = 'n' ] || [ $repo = 'no' ]
     then
@@ -105,65 +204,8 @@ Deploy()
             clear
             cd $path
 
-            #duplicate & rename config file to project name
-            sudo cp config $projectname
+            Installation
 
-            #templating modification in config file
-            sed -i "s/SERVERNAME/$hostip/" $projectname
-            sed -i "s/PROJECTNAME/$projectname/" $projectname
-
-            #create folder insite html folder & add permission of nginx to read folder
-            sudo mkdir /var/www/html/$projectname > /dev/null 2>&1
-            sudo chown -R www-data:www-data /var/www/html/$projectname > /dev/null 2>&1
-
-            #move config file to sites-available
-            sudo cp $projectname /etc/nginx/sites-available/ > /dev/null 2>&1
-
-            #move build project to html folder
-            sudo cp -a "${projectpath}/build/." /var/www/html/$projectname > /dev/null 2>&1
-
-            #make link between sites-available & site enabled
-            sudo ln -s /etc/nginx/sites-available/$projectname /etc/nginx/sites-enabled > /dev/null 2>&1
-
-            #delete default conf of nginx
-            sudo rm -f /etc/nginx/sites-available/default > /dev/null 2>&1
-            sudo rm -f /etc/nginx/sites-enabled/default > /dev/null 2>&1
-
-            #delete config file in current folder
-            sudo rm $projectname
-
-            echo -e "\033[0;32m
-
-    _____ _    _  _____ _____ ______  _____ _____ 
-    / ____| |  | |/ ____/ ____|  ____|/ ____/ ____|
-    | (___ | |  | | |   | |    | |__  | (___| (___  
-    \___ \| |  | | |   | |    |  __|  \___ \\___ \ 
-    ____) | |__| | |___| |____| |____ ____) |___) |
-    |_____/ \____/ \_____\_____|______|_____/_____/ 
-                                                    
-                                                    
-                                        
-    \033[0;0m"
-
-            echo -e "File has been created in \033[0;36m/etc/nginx/sites-available/$projectname\033[0;0m"
-            echo -e "Your website folder is located in \033[0;36m/var/www/html/$projectname\033[0;0m"
-            echo -e "Link has been make in \033[0;36m/etc/nginx/site-enabled/$projectname\033[0;0m"
-            echo ""
-
-            sleep 1
-            
-            echo "Testing config for nginx..."
-            #test if config of nginx is correct or not
-            sudo nginx -t > /dev/null 2>&1
-            if [ $? -eq 0 ]
-            then
-                #if config is ok, restart nginx
-                sudo systemctl restart nginx > /dev/null 2>&1 
-                echo -e "\033[0;32mSUCCESS! Go to http://${hostip}/ to see you website\033[0;0m"
-            else 
-                echo -e "\033[0;31mERROR: Config Failed\033[0;0m"
-                exit 1
-            fi
         else
             echo -e "\033[0;31mWrong IP pattern\033[0;0m"
             exit 1
@@ -181,39 +223,15 @@ Deploy()
 
     if [ $sslresponse = 'Y' ] || [ $sslresponse = 'Yes' ] || [ $sslresponse = 'y' ] || [ $sslresponse = 'yes' ]
     then
-        clear
 
-        echo "Installing packages ..."
-        sudo apt install certbot python3 python3-certbot-nginx -y > /dev/null 2>&1
-
-        echo "Put your Host Domain Name (eg: www.example.com)"
-        read dns
-
-        #edit server name in sites-available folder of nginx
-        sed -i "s/$hostip/$dns/" /etc/nginx/sites-available/$projectname > /dev/null 2>&1
-
-        #use certbot to link dns & to put HTTPS
-        sudo certbot --nginx -d $dns
-        if [ $? -eq 0 ]
-        then
-            clear
-            #restart nginx
-            echo "Restarting nginx..."
-            sudo systemctl restart nginx > /dev/null 2>&1
-
-            clear
-            echo -e "\033[0;32mSuccess !\033[0;0m go to \033[0;32mhttps://${dns}/\033[0;0m"
-            echo -e "You can test the config at \033[0;32mhttps://www.ssllabs.com/ssltest/analyze.html?d=${dns}&latest\033[0;0m"
-        else
-            echo -e "\033[0;31mError : unable to setup certbot certificate, try again.\033[0;0m"
-            exit 1
-        fi
+        SSLSetup
 
     else
         echo "Exit"
-        exit
+        exit 0
     fi
 }
+
 Delete()
 {
     #Listing all folder insite projects
